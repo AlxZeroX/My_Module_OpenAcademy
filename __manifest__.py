2a064a9fb9e752ae569a00562b2e340dd409776f
Cristian Alexander Martinez <40245786+AlxZeroX@users.noreply.github.com>
2021-10-15 12:40:55 -0600

    Name, summary, description, data, etc.

--- __manifest__.py
+++ __manifest__.py
@@ -1,32 +1,45 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "open_acaed",
+    'name': "Open Academy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo de Cursos""",
 
     'description': """
-        Long description of module's purpose
+        Open Academy module for managing trainings:
+            - training course
+            - training session
+            - attendees registration
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': 'Cristian Alexander Martinez',
+    'company': 'ZeroCorp',
+    'website': 'https://github.com/AlxZeroX',
+    'live_test_url': 'https://github.com/AlxZeroX',
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Courses',
+    'version': '14.0.14.10.21',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board', 'website_slides'],
+    'images': ['static/description/banner.gif'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'data/slide_channel_data.xml',
+        'data/slide_channel_data_v13.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
